2c63db64e269d0923cff5f3f6cb7dba062afa6d9
engineeralok <engineeralok07@gmail.com>
2025-01-22 06:11:45 +0530

    Git deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/panam-properties/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
